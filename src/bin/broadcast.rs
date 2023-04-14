40b623f5415b84d2a286dc1b2e367bd6ebc50eff
Can Ivit <canivit@users.noreply.github.com>
2023-04-13 21:14:34 -0400

    solve broadcast part 2

--- src/bin/broadcast.rs
+++ src/bin/broadcast.rs
@@ -1,3 +1,5 @@
+use std::collections::{HashMap, HashSet};
+
 use anyhow::Context;
 use maelstrom::{run_node, Message, Node, TrailingLineSerializer};
 use serde::{Deserialize, Serialize};
@@ -5,46 +7,51 @@ use serde::{Deserialize, Serialize};
 #[derive(Serialize, Deserialize)]
 #[serde(tag = "type")]
 #[serde(rename_all = "snake_case")]
-enum GeneratePayload {
-    Generate,
-    GenerateOk { id: u64 },
-}
-
-#[derive(Serialize, Deserialize)]
-#[serde(tag = "type")]
-#[serde(rename_all = "snake_case")]
 enum Payload {
-    Broadcast { message: usize },
+    Broadcast {
+        message: usize,
+    },
     BroadcastOk,
     Read,
-    ReadOk { messages: Vec<usize> },
-    Topology,
+    ReadOk {
+        messages: HashSet<usize>,
+    },
+    Topology {
+        topology: HashMap<String, Vec<String>>,
+    },
     TopologyOk,
+    Gossip {
+        message: usize,
+    },
 }
 
-struct UniqueNode<W>
+struct BroadcastNode<W>
 where
     W: std::io::Write,
 {
+    node_ids: Vec<String>,
     msg_id: usize,
     serializer: TrailingLineSerializer<W>,
-    store: Vec<usize>,
+    store: HashSet<usize>,
+    topology: HashMap<String, Vec<String>>,
 }
 
-impl<W> Node<W, Payload> for UniqueNode<W>
+impl<W> Node<W, Payload> for BroadcastNode<W>
 where
     W: std::io::Write,
 {
     fn new(
         _node_id: String,
-        _node_ids: Vec<String>,
+        node_ids: Vec<String>,
         msg_id: usize,
         serializer: TrailingLineSerializer<W>,
     ) -> Self {
         Self {
+            node_ids,
             msg_id,
             serializer,
-            store: Vec::new(),
+            store: HashSet::new(),
+            topology: HashMap::new(),
         }
     }
 
@@ -55,11 +62,7 @@ where
         let mut reply = msg.into_reply(self.msg_id);
         match reply.body.payload {
             Payload::Broadcast { message } => {
-                self.store.push(message);
-                reply.body.payload = Payload::BroadcastOk;
-                self.serializer
-                    .serialize(&reply)
-                    .context("failed to serialize broadcast_ok message")?;
+                self.handle_broadcast(reply, message)?;
             }
 
             Payload::Read => {
@@ -73,13 +76,20 @@ where
                     anyhow::bail!("failed to recover store after move")
                 };
                 self.store = messages;
+                self.msg_id += 1;
             }
 
-            Payload::Topology => {
+            Payload::Topology { topology } => {
+                self.topology = topology;
                 reply.body.payload = Payload::TopologyOk;
                 self.serializer
                     .serialize(&reply)
                     .context("failed to serialize topology_ok message")?;
+                self.msg_id += 1;
+            }
+
+            Payload::Gossip { message } => {
+                self.store.insert(message);
             }
 
             Payload::BroadcastOk => anyhow::bail!("received unexpected broadcast_ok message"),
@@ -87,13 +97,42 @@ where
             Payload::TopologyOk => anyhow::bail!("received unexpected topology_ok message"),
         }
 
-        self.msg_id += 1;
         Ok(self)
+    }
+}
+
+impl<W> BroadcastNode<W>
+where
+    W: std::io::Write,
+{
+    fn handle_broadcast(
+        &mut self,
+        mut reply: Message<Payload>,
+        message: usize,
+    ) -> anyhow::Result<()> {
+        self.store.insert(message);
+
+        reply.body.payload = Payload::BroadcastOk;
+        self.serializer
+            .serialize(&reply)
+            .context("failed to serialize broadcast_ok message")?;
+        self.msg_id += 1;
+
+        reply.body.payload = Payload::Gossip { message };
+        for neighbor in &self.node_ids {
+            reply.dst = neighbor.clone();
+            reply.body.msg_id = self.msg_id;
+            self.serializer
+                .serialize(&reply)
+                .context("failed to serialize gossip message")?;
+            self.msg_id += 1;
+        }
+        Ok(())
     }
 }
 
 fn main() -> anyhow::Result<()> {
     let reader = std::io::stdin().lock();
     let writer = std::io::stdout().lock();
-    run_node::<UniqueNode<_>, _, _, _>(reader, writer)
+    run_node::<BroadcastNode<_>, _, _, _>(reader, writer)
 }
